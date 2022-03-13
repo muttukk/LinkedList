bd4370639e0feac2f48655b46286a2942f857af9
muttukk <muttukk@gmail.com>
2022-03-13 02:21:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// Create the below linked list:
+// myLinkedList = {
+//   head: {
+//     value: 10
+//     next: {
+//       value: 5
+//       next: {
+//         value: 16
+//         next: null
+//       }
+//     }
+//   }
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length++;
+  }
+  append(value) {
+    const newNode={
+      value:value,
+      next:null
+    }         
+    this.tail.next=newNode;
+    this.tail=newNode;
+    return this;
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+console.log (myLinkedList);
